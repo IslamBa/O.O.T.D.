16ddd9ea71cd53e59ce518ed149988dfa94de8df
Islam baj <dk.baj95@gmail.com>
2018-09-21 21:31:41 +0200

    Eigener vorläufiger Login und Signup

--- Code/app/client/main.js
+++ Code/app/client/main.js
@@ -3,4 +3,32 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Template.register.events({
+    'submit form'(event, template){
+        event.preventDefault();
+        var username = $("#username").val();
+        var email = $("#email").val();
+        var passwort = $("#password").val();
+        Accounts.createUser({
+            username: username,
+            email: email,
+            password: passwort
+        });
+    }
+});
 
+Template.login.events({
+    'submit form'(event, template){
+        event.preventDefault();
+        var username = $("#login-username").val();
+        var passwort = $("#login-password").val();
+        
+        Meteor.loginWithPassword(username, passwort);
+    }
+});
+
+Template.content.events({
+    'click .btnLogout'(event){
+        Meteor.logout();
+    }
+});
