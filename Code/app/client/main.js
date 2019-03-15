aea6d694d1ed2386902d61e920a0945e735bd56d
Islam baj <dk.baj95@gmail.com>
2019-03-15 12:06:51 +0100

    Verbesserungen

--- Code/app/client/main.js
+++ Code/app/client/main.js
@@ -399,58 +399,6 @@ Template.content.onRendered(() => {
     }
     notification();
 
-    //Wetteranimation
-    // function animation(){
-
-    //     var c = document.getElementById("c"),
-    //             ctx = c.getContext("2d");
-
-    //     c.width = 350;
-    //     c.height = 750;
-
-    //     var lines = [],
-    //             maxSpeed = 15,
-    //             spacing = 15,
-    //             xSpacing = 0,
-    //             n = innerWidth / spacing,
-    //             colors = ["#0000ff", "#0000ff", "#0000ff", "#0000ff"],
-    //             i;
-
-    //     for (i = 0; i < n; i++){
-    //         xSpacing += spacing;
-    //         lines.push({
-    //             x: xSpacing,
-    //             y: Math.round(Math.random()*c.height),
-    //             width: 2,
-    //             height: Math.round(Math.random()*(innerHeight/10)),
-    //             speed: Math.random()*maxSpeed + 10,
-    //             color: colors[Math.floor(Math.random() * colors.length)]
-    //         });
-    //     }
-
-
-    //     function draw(){
-    //         var i;
-    //         ctx.clearRect(0,0,c.width,c.height);
-
-    //         for (i = 0; i < n; i++){
-    //             ctx.fillStyle = lines[i].color;
-    //             ctx.fillRect(lines[i].x, lines[i].y, lines[i].width, lines[i].height);
-    //             lines[i].y += lines[i].speed;
-
-    //             if (lines[i].y > c.height)
-    //                 lines[i].y = 0 - lines[i].height;
-    //         }
-
-    //         requestAnimationFrame(draw);
-
-    //     }
-
-    //     draw();
-
-    // };
-    // animation();
-
     Meteor.call('checkFavorite', (error, result) => {
         if (result == false) {
             console.log("boiidjhf2");
@@ -622,7 +570,7 @@ Template.content.events({
         Meteor.call('checkFavorite', (error, result) => {
             if (result == false) {
                 console.log("boiidjhf");
-                console.log(this);
+                // console.log(this);
             }
             else {
                 Meteor.call('addFavorite', (error, result) => {
